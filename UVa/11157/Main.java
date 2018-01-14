5db5a4167f634b62f225ad350e9cecc5da208d94
ThePinger <omaremadsabry@gmail.com>
2018-01-14 21:38:08 +0200

    Edited 11157

--- UVa/11157/Main.java
+++ UVa/11157/Main.java
@@ -22,67 +22,46 @@ public class Main
             st = new StringTokenizer(br.readLine());
 
             boolean turn  = false;
-            long maxLeap1 = 0;
-            long maxLeap2 = 0;
-            long last1    = 0;
-            long last2    = 0;
-            Stack<Long> returnRoute1 = new Stack<>();
-            Stack<Long> returnRoute2 = new Stack<>();            
-            returnRoute1.push((long) 0);
-            returnRoute2.push((long) 0);
+            long maxLeap = 0;
+            long last    = 0;
+            Stack<Long> returnRoute = new Stack<>();
+            returnRoute.push((long) 0);
             while(st.hasMoreTokens())
             {
                 String[] s = st.nextToken().split("-");
                 Long tmp = Long.parseLong(s[1]);
                 if(s[0].equals("B"))
                 {
-                    returnRoute1.push(tmp);
-                    returnRoute2.push(tmp);
-                    maxLeap1 = Math.max(maxLeap1, tmp - last1);
-                    maxLeap2 = Math.max(maxLeap2, tmp - last2);
-                    last1 = tmp;
-                    last2 = tmp;
+                    returnRoute.push(tmp);
+                    maxLeap = Math.max(maxLeap, tmp - last);
+                    last = tmp;
                 }
                 else
                 {
                     if(turn)
                     {
-                        returnRoute2.push(tmp);
-                        maxLeap1 = Math.max(maxLeap1, tmp - last1);
-                        last1 = tmp;
+                        maxLeap = Math.max(maxLeap, tmp - last);
+                        last = tmp;
                         turn  = false;
                     }
                     else
                     {
-                        returnRoute1.push(tmp);
-                        maxLeap2 = Math.max(maxLeap2, tmp - last2);
-                        last2 = tmp;
+                        returnRoute.push(tmp);
                         turn  = true;
                     }
                 }
 
             }
 
-            maxLeap1 = Math.max(maxLeap1, distance - last1);
-            maxLeap2 = Math.max(maxLeap2, distance - last2);
+            maxLeap = Math.max(maxLeap, distance - last);
             
-            last1 = distance;
-            while(!returnRoute1.isEmpty())
+            last = distance;
+            while(!returnRoute.isEmpty())
             {
-                long tmp = returnRoute1.pop();
-                maxLeap1 = Math.max(maxLeap1, last1 - tmp);
-                last1 = tmp;
+                long tmp = returnRoute.pop();
+                maxLeap = Math.max(maxLeap, last - tmp);
+                last = tmp;
             }
-
-            last2 = distance;
-            while(!returnRoute2.isEmpty())
-            {
-                long tmp = returnRoute2.pop();
-                maxLeap2 = Math.max(maxLeap1, last2 - tmp);
-                last2 = tmp;
-            }
-
-            long maxLeap = Math.min(maxLeap1, maxLeap2);
             sb.append("Case " + (t++) + ": " + maxLeap + "\n");
         }
         System.out.print(sb);
