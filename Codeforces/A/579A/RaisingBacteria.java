c1c9461b95502df7303b40150d16cbd7e4c8651d
ThePinger <omaremadsabry@gmail.com>
2018-01-22 01:44:33 +0200

    579A

---
+++ Codeforces/A/579A/RaisingBacteria.java
@@ -0,0 +1,13 @@
+//579A
+//Raising Bacteria
+
+import java.util.*;
+
+public class RaisingBacteria
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.bitCount(sc.nextInt()));    
+    }
+}
