dcecef4bb8a5cdce6457a924a1ef36b5d72199f4
ThePinger <omaremadsabry@gmail.com>
2018-01-22 16:03:46 +0200

    Removed unused Part

--- UVa/10626/Main.java
+++ UVa/10626/Main.java
@@ -39,47 +39,6 @@ public class Main
         return dp[cokes][fiv][ten] = Math.min(Math.min(takeT, takeFOne), Math.min(Math.min(takeTOne, takeOne), take2F));
     }
 
-    /*public static void fill()
-    {
-        for(int fiv = 0; fiv < five+ Math.min(ones/3, tens) + 1; fiv++)
-            for(int ten = 0; ten < tens + 1; ten++)
-            {
-                int remS = sum - (fiv * 5) - (ten * 10);                
-                
-                if(ten > 0)
-                    dp[coke][fiv][ten] = 1;
-                else if(fiv > 1)
-                    dp[coke][fiv][ten] = 2;
-                else if(remS > 2 && fiv > 0)
-                    dp[coke][fiv][ten] = 4;
-                else if(remS > 7)
-                    dp[coke][fiv][ten] = 8;  
-            }
-
-        for(int cok = coke - 1; cok > -1; cok--)
-            for(int fiv = 0; fiv < five + Math.min(ones/3, tens) + 1; fiv++)
-                for(int ten = 0; ten < tens + 1; ten++)
-                {
-                    int remS = sum - (8 * (coke - cok)) - (fiv * 5) - (ten * 10);
-                    int takeT    = max;
-                    int takeFOne = max;
-                    int takeTOne = max;
-                    int takeOne  = max;
-                    int take2F   = max;
-
-                    if(ten > 0) takeT = 1 + dp[cok + 1][fiv][ten - 1];
-                    if(fiv > 0 && remS > 2) takeFOne = 4 + dp[cok + 1][fiv - 1][ten];
-                    if(ten > 0 && remS > 2) takeTOne = 4 + dp[cok + 1][fiv + 1][ten - 1];
-                    if(remS > 7) takeOne = 8 + dp[cok + 1][fiv][ten];
-                    if(fiv > 1) take2F = 2 + dp[cok + 1][fiv - 2][ten];
-
-                    if(takeT == max && takeFOne == max && takeTOne == max && takeOne == max && take2F == max)
-                        continue;
-
-                    dp[cok][fiv][ten] = Math.min(Math.min(takeT, takeFOne), Math.min(Math.min(takeTOne, takeOne), take2F));             
-                }                 
-    }*/
-
     public static void main(String[] args)throws IOException 
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));    
