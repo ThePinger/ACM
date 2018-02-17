a39679538114522f0c0618bc7f364afc77dd6a60
ThePinger <omaremadsabry@gmail.com>
2018-02-17 19:42:34 +0200

    938B

---
+++ Codeforces/B/938B/B.java
@@ -0,0 +1,118 @@
+
+import java.util.*;
+import java.io.*;
+
+public class B
+{
+    public static void main(String[] args) throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        PrintWriter pw = new PrintWriter(System.out);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int me = 1;
+        int friend = 1000000;
+        int time = 0;
+        int picked = 0;
+        int i = 0;
+        int j = n - 1;
+
+        while(picked < n)
+        {
+            if(me == arr[i])
+            {
+                picked++;
+                i++;
+            }
+            if(friend == arr[j] && me != arr[j])
+            {
+                picked++;
+                j--;
+            }
+
+            if(picked == n) break;
+            me++;
+            friend--;
+            time++;
+        }
+        
+        pw.println(time);
+        pw.flush();
+        pw.close();
+    }
+
+    static class Scanner
+	{
+		StringTokenizer st;
+		BufferedReader br;
+
+		public Scanner(InputStream s)
+		{
+			br = new BufferedReader(new InputStreamReader(s));
+		}
+
+		public Scanner(String s) throws FileNotFoundException
+		{
+			br = new BufferedReader(new FileReader(new File((s))));
+		}
+
+		public String next() throws IOException
+		{
+			while (st == null || !st.hasMoreTokens())
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+
+		public int nextInt() throws IOException
+		{
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() throws IOException
+		{
+			return Long.parseLong(next());
+		}
+
+		public String nextLine() throws IOException
+		{
+			return br.readLine();
+		}
+
+		public double nextDouble() throws IOException
+		{
+			String x = next();
+			StringBuilder sb = new StringBuilder("0");
+			double res = 0, f = 1;
+			boolean dec = false, neg = false;
+			int start = 0;
+			if (x.charAt(0) == '-')
+			{
+				neg = true;
+				start++;
+			}
+			for (int i = start; i < x.length(); i++)
+				if (x.charAt(i) == '.')
+				{
+					res = Long.parseLong(sb.toString());
+					sb = new StringBuilder("0");
+					dec = true;
+				} else
+				{
+					sb.append(x.charAt(i));
+					if (dec)
+						f *= 10;
+				}
+			res += Long.parseLong(sb.toString()) / f;
+			return res * (neg ? -1 : 1);
+		}
+
+		public boolean ready() throws IOException
+		{
+			return br.ready();
+        }
+    }
+}
