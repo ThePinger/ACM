6f7946465c49c08c4b09af0131bd09acde0985f1
ThePinger <omaremadsabry@gmail.com>
2018-03-25 21:48:16 +0200

    221A

---
+++ Codeforces/A/221A/LittleElephantFunction.java
@@ -0,0 +1,18 @@
+//221A
+//Little Elephant and Function
+
+import java.util.Scanner;
+
+public class LittleElephantFunction
+{
+    
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(n);
+
+        for(int i = 1; i < n; i++)
+            System.out.print(" " + i);
+    }
+}
