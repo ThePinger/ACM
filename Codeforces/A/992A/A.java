d4539cd67cee5ac5a103e9fa08cd0079e937c182
ThePinger <omaremadsabry@gmail.com>
2018-06-18 22:09:47 +0200

    992A

---
+++ Codeforces/A/992A/A.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A 
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		boolean[] vis = new boolean[200001];
+		int diff = 0;
+		for(int i = 0; i < n; i++)
+		{
+			int tmp = sc.nextInt();
+			if(!vis[tmp + 100000] && tmp != 0)
+			{
+				vis[tmp + 100000] = true;
+				diff++;
+			}
+		}
+		
+		System.out.println(diff);
+	}
+
+	static class Scanner 
+	{
+		StringTokenizer st;
+		BufferedReader br;
+	
+		public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+	
+		public String next() throws IOException 
+		{
+			while (st == null || !st.hasMoreTokens()) 
+				st = new StringTokenizer(br.readLine());
+			return st.nextToken();
+		}
+	
+		public int nextInt() throws IOException {return Integer.parseInt(next());}
+		
+		public long nextLong() throws IOException {return Long.parseLong(next());}
+	
+		public String nextLine() throws IOException {return br.readLine();}
+		
+		public double nextDouble() throws IOException
+		{
+			String x = next();
+			StringBuilder sb = new StringBuilder("0");
+			double res = 0, f = 1;
+			boolean dec = false, neg = false;
+			int start = 0;
+			if(x.charAt(0) == '-')
+			{
+				neg = true;
+				start++;
+			}
+			for(int i = start; i < x.length(); i++)
+				if(x.charAt(i) == '.')
+				{
+					res = Long.parseLong(sb.toString());
+					sb = new StringBuilder("0");
+					dec = true;
+				}
+				else
+				{
+					sb.append(x.charAt(i));
+					if(dec)
+						f *= 10;
+				}
+			res += Long.parseLong(sb.toString()) / f;
+			return res * (neg?-1:1);
+		}
+		
+		public boolean ready() throws IOException {return br.ready();}
+	
+	
+	}
+}
