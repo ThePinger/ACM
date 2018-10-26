bba58c6ef5f22897665ff6e2e55db5a6ab5ede80
ThePinger <omaremadsabry@gmail.com>
2018-10-26 13:53:59 +0200

    1073B

---
+++ Codeforces/B/1073B/B.java
@@ -0,0 +1,94 @@
+
+import java.io.*;
+import java.util.*;
+
+public class B
+{
+	
+	public static void main(String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(System.out);
+		int n = sc.nextInt();
+		int[] stack = new int[n];
+		int[] pos = new int[n + 1];
+		
+		for(int i = 0; i < n; i++)
+		{
+			int tmp = sc.nextInt();
+			stack[i] = tmp;
+			pos[tmp] = i;
+		}
+		
+		int point = 0;
+		for(int i = 0; i < n; i++)
+		{
+			int rem = sc.nextInt();
+			if(point > pos[rem])
+				pw.print(0 + " ");
+			else
+			{
+				pw.print(pos[rem] - point + 1 + " ");
+				point = pos[rem] + 1;
+			}
+		}
+		
+		pw.println();
+		pw.flush();
+		pw.close();
+	}
+
+	static class Scanner 
+    {
+        StringTokenizer st;
+        BufferedReader br;
+
+        public Scanner(InputStream s){	br = new BufferedReader(new InputStreamReader(s));}
+
+        public String next() throws IOException 
+        {
+            while (st == null || !st.hasMoreTokens()) 
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+
+        public int nextInt() throws IOException {return Integer.parseInt(next());}
+        
+        public long nextLong() throws IOException {return Long.parseLong(next());}
+
+        public String nextLine() throws IOException {return br.readLine();}
+        
+        public double nextDouble() throws IOException
+        {
+            String x = next();
+            StringBuilder sb = new StringBuilder("0");
+            double res = 0, f = 1;
+            boolean dec = false, neg = false;
+            int start = 0;
+            if(x.charAt(0) == '-')
+            {
+                neg = true;
+                start++;
+            }
+            for(int i = start; i < x.length(); i++)
+                if(x.charAt(i) == '.')
+                {
+                    res = Long.parseLong(sb.toString());
+                    sb = new StringBuilder("0");
+                    dec = true;
+                }
+                else
+                {
+                    sb.append(x.charAt(i));
+                    if(dec)
+                        f *= 10;
+                }
+            res += Long.parseLong(sb.toString()) / f;
+            return res * (neg?-1:1);
+        }
+        
+        public boolean ready() throws IOException {return br.ready();}
+
+    }
+}
+
